ba713e11efb666a6f8683386895bc7dfafa7dd60
LuckyDL <dl.zhang@hzgosun.com>
2018-09-21 16:57:53 +0800

    init project

---
+++ src/main/java/com/luckyDL/ftpcameldemo/FtpCamelDemoApplication.java
@@ -0,0 +1,12 @@
+package com.luckyDL.ftpcameldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpCamelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpCamelDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/luckyDL/ftpcameldemo/FtpCamelDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luckyDL.ftpcameldemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtpCamelDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
